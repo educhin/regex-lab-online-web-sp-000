4fb70110c41af0183f4d56aa3658ef3b5b787b1b
Chinedu Arinze <c.s.arinze@gmail.com>
2019-03-22 01:48:47 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,17 +1,26 @@
 def starts_with_a_vowel?(word)
-
+  if word.scan(/\b[aeiouAEIOU]\w+/) != []
+    return true
+  else
+    return false
+  end
 end
 
-def words_starting_with_un_and_ending_with_ing(text)
 
+def words_starting_with_un_and_ending_with_ing(text)
+  text.scan(/\b[unUN]+[ingING]\w+/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  if text.scan(/\A[A-Z]+\z[:punct:]/) != []
+    return true
+  else
+    return false
+  end
 end
 
 def valid_phone_number?(phone)
